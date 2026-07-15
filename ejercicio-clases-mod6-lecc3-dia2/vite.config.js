c7b8afc7b79c837de777d029630fb7d744208499
zakkdruzer <zakkdruzer@gmail.com>
2026-07-14 23:54:24 -0300

    fix: se ajusta vite.config.js para visualizacion de gitpages-3

--- ejercicio-clases-mod6-lecc3-dia2/vite.config.js
+++ ejercicio-clases-mod6-lecc3-dia2/vite.config.js
@@ -3,5 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/cv2-modulo-6-leccion-3/', // ojo: termina con /
+  base: '/vue-ejerc-dual/', // ojo: termina con /
 })
